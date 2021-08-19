ee5d84cf44d01ba688275acd0ea531c0ac013216
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-08-19 11:37:10 +0100

    Display route parameter in component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Navbar from "./component/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Home from "./component/Home";
 import About from "./component/About";
 import Contact from "./component/Contact";
+import Post from "./component/Post";
 
 class App extends Component {
   render() {
@@ -15,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
         </div>
       </Router>
     );
