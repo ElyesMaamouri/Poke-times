a961e8b1e6ca081c473cc68a5ded59015d23aca0
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-08-19 13:33:06 +0100

    Add router parameter

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 class Post extends Component {
   state = {
-    id: null,
+    post: null,
   };
+
   componentDidMount() {
     console.log("data from Route in app.js", this.props);
     let id = this.props.match.params.post_id;
-    this.setState({
-      id: id,
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/" + id)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          post: res.data,
+        });
+      });
   }
   render() {
-    return (
-      <div className="container">
-        <h4>{this.state.id}</h4>
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
       </div>
+    ) : (
+      <h2 className="center">Loading post ....</h2>
     );
+    return <div className="container">{post}</div>;
   }
 }
 
