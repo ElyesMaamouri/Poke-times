9ed82e607559df164c47ef33704e4822b5a211e9
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-08-20 19:36:33 +0100

    Connect component to redux & pass state to props component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Home extends Component {
-  state = {
-    posts: [],
-  };
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      this.setState({ posts: res.data.slice(0, 10) });
-    });
-  }
+  // state = {
+  //   posts: [],
+  // };
+  // componentDidMount() {
+  //   axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+  //     this.setState({ posts: res.data.slice(0, 10) });
+  //   });
+  // }
   render() {
-    const { posts } = this.state;
+    const posts = this.props.post;
+    // console.log("props", posts);
     const postList = posts.length ? (
       posts.map((item) => {
+        console.log("all posts", item.title, item.body);
         return (
           <div className="post card" key={item.id}>
             <div className="card-content">
@@ -38,4 +41,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    post: state.post,
+  };
+};
+
+export default connect(mapStateToProps)(Home);
